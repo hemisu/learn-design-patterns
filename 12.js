c26849edd19bb9f5986e9bac23d2ffc480db5181
hemisu <hekunyu@me.com>
2018-12-05 16:35:59 +0800

    chapter 9-12

---
+++ 12.js
@@ -0,0 +1,15 @@
+var objectPoolFactory = function(createObjFn) {
+  var objectPool = [];
+
+  return {
+    create: function() {
+      var obj = objectPool.length === 0 ?
+        createObjFn.apply(this, arguments) : objectPool.shift();
+
+        return obj;
+    },
+    recover: function(obj) {
+      objectPool.push(obj);
+    }
+  }
+}
